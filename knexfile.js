280b9f8487a0e4c9c4338c856c4ac86bc5ca64e0
Andrew <acarmstrong98@gmail.com>
2019-04-06 13:56:07 -0400

    added migrations

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,18 @@ module.exports = {
       connection: {
         filename: './salesforce.sqlite3'
       },
+
       useNullAsDefault: true,
       migrations: {
         directory: './data/migrations',
       },
+
+      pool: {
+        afterCreate: (conn, done) => {
+          conn.run("PRAGMA foreign_keys = ON", done);
+        }
+      }
+      
     },
   
   };
